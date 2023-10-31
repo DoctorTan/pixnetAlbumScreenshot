5491d832c27412de29d3595e0761b7fe60c3e9c7
DoctorTan <87746911+DoctorTan@users.noreply.github.com>
2023-10-31 18:33:35 +0800

    實現保存所有相冊的時候生成用戶名

--- index.js
+++ index.js
@@ -2,8 +2,72 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const fs = require('fs');
 
-async function getScreenHot(link) {
-  let i = 0
+let link = 'https://abbey1117.pixnet.net/album/list'
+
+let i = 0
+async function getAlbumList(link) {
+
+  //开始时间
+  const startTime = new Date();
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(link);
+  // 返回一个包含相册名和相册连接的
+  let albumInfoList = await page.evaluate(async () => {
+    // 获取用户名字
+    const userAlbumName = document.querySelector('meta[name="description"]').getAttribute('content');
+    let linkList = []
+    // 获取所有类名为 "photolink" 的 DOM 节点
+    const photoLinks = document.querySelectorAll('.album-grid-list .grid-photo .photolink');
+    // 遍历每个节点并获取其 href 属性值
+    photoLinks.forEach((node) => {
+      const href = node.getAttribute('href');
+      linkList.push(href)
+    });
+    //获取文件名并创建文件夹
+    let AlbumNamelist = []
+    const AlbumNameNodes = document.querySelectorAll('.album-grid-list .grid-photo .photolink .thumb');
+    AlbumNameNodes.forEach((node) => {
+      const alt = node.getAttribute('alt');
+      AlbumNamelist.push(alt)
+    });
+    // console.log(AlbumNamelist);
+    return { linkList, AlbumNamelist, userAlbumName }; // 获取 href 属性值
+  })
+  albumInfoList.userAlbumName = filterPathName(albumInfoList.userAlbumName)
+  const useDir = path.join(process.cwd(), albumInfoList.userAlbumName);
+  if (!fs.existsSync(useDir)) {
+    fs.mkdirSync(useDir);
+    console.log(`创建用户文件夹:   ${useDir}  成功`);
+  }
+  //这里已经获取相册列表
+  let AlbumPromise = []
+  for (let i = 0; i < albumInfoList.AlbumNamelist.length; i++) {
+    // 创建文件夹
+    const albumName = filterPathName(albumInfoList.AlbumNamelist[i])
+    const dir = path.join(process.cwd(), albumInfoList.userAlbumName, albumName);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+      console.log(`创建文件夹:   ${dir}  成功`);
+    }
+    //实现相册截图
+    AlbumPromise.push(getScreenHot(albumInfoList.linkList[i], albumInfoList.userAlbumName))
+  }
+  // console.log(AlbumPromise);
+  Promise.all(AlbumPromise).then(res => {
+    const endTimeCollect = new Date();
+    const timeTaken = endTimeCollect - startTime;
+    console.log('全部执行完,花费', timeTaken / 1000, '秒');
+  })
+  browser.close()
+
+}
+
+
+async function getScreenHot(link, userAlbumName, index) {
+  if (!index) {
+    index = 1
+  }
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setViewport({
@@ -13,43 +77,41 @@ async function getScreenHot(link) {
 
   });
   await page.goto(link);
-  // await page.focus("#kw")
-
-  // await page.keyboard.sendCharacter('陈明筠')
-  // await page.keyboard.down('Enter');
-
-
-  const dir = path.join(process.cwd(), '图啊啊');
-
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-    console.log(`创建文件夹:   ${dir}  成功`);
-  }
-  // 相册名字
-  //获取下一页的页面链接
   let { albumName, nextlink } = await page.evaluate(async () => {
     const element = document.querySelector('.nextBtn'); // 使用类名选择器获取元素
-    const albumName = document.querySelector('meta[name="description"]').getAttribute('content');
+    // const albumName = document.querySelector('meta[name="description"]').getAttribute('content');
+    const breadcrumbElement = document.querySelector('#breadcrumb');
+    const secondLink = breadcrumbElement.querySelectorAll('a')[1];
+    const albumName = secondLink.innerText;
+    console.log(albumName);
+    // const albumName = secondLink.textContent;
     return { nextlink: element.href, albumName }; // 获取 href 属性值
-
   })
-  if (nextlink) {
-    console.log('立刻执行');
-    getScreenHot(nextlink)
-  }
 
-
+  //处理特殊字符
+  albumName = filterPathName(albumName)
+  const dir = path.join(process.cwd(), userAlbumName, albumName);
+  // if (!fs.existsSync(dir)) {
+  //   fs.mkdirSync(dir);
+  //   console.log(`创建文件夹:   ${dir}  成功`);
+  // }
 
   //准备截图
   await autoScroll(page)
   await page.screenshot({
-    path: `${dir}/${filterPathName(albumName)}--${++i}.png`, fullPage: true
+    path: `${dir}/${albumName}-${index}.png`, fullPage: true
   });
+  console.log(`截图${albumName}-${index}成功`);
   await browser.close()
+  if (nextlink) {
+    // console.log('立刻执行');
+    await getScreenHot(nextlink, userAlbumName, ++index)
+  }
+  return 'ok'
 }
-getScreenHot('https://abbey1117.pixnet.net/album/set/14245552')
+// getScreenHot('https://abbey1117.pixnet.net/album/set/14245552')
 // getScreenHot('https://abbey1117.pixnet.net/album/set/14245552?after=104096618')
-
+getAlbumList(link)
 
 
 
